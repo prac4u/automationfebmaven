a35422729be70a9f133d3e2bc3dcfbfb1238a14f
pra4u <palkar.p@gamail.com>
2023-06-16 07:43:11 +0530

    unwanted import statements removed and one log line added

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -1,28 +1,18 @@
 package testbase;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
 import driverfactory.DriverFactory;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.PropConfig;
 import utils.ScreenshotUtil;
 public class TestBase {
@@ -86,7 +76,7 @@ public class TestBase {
 		
 		if(result.getStatus() == ITestResult.FAILURE)
 		{
-			
+			log.info("Test case failed " + testcaseName);
 			String fileName =testcaseName + ".png";
 			ScreenshotUtil.takeScreenshot(driver, fileName);
 		}
