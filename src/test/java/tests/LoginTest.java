955615a4a8fd41fc4c00aee428583d9578d0d87e
pra4u <palkar.p@gamail.com>
2023-06-17 08:45:25 +0530

    Profile Page and test class added

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,7 +3,6 @@ package tests;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import pages.LoginPage;
 import testbase.TestBase;
